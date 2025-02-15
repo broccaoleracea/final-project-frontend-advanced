c30f690201eeac2f0e9036d68f370cc9b34922ea
dausandhika <firdaus.andhika06@gmail.com>
2025-02-15 20:15:19 +0700

    saya menambahkan sesuatu

--- .history/src/components/sidebar_20250215201306.tsx
+++ .history/src/components/sidebar_20250215201306.tsx
@@ -4,7 +4,7 @@ const Sidebar = () => {
   return (
     <aside
       id="logo-sidebar"
-      className="fixed top-0 left-0 z-40 w-64 h-screen overflow-y-auto bg-[#1b162d] shadow-lg"
+      className="fixed top-0 left-0 z-40 w-64 h-screen overflow-y-auto bg-[#1b162d  ] shadow-lg"
       aria-label="Sidebar"
     >
       {/* Background dengan efek shadow */}
