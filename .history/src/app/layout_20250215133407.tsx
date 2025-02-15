aefb5e8f01a8d2fd687abaa0baada7bbe47be6e8
dausandhika <firdaus.andhika06@gmail.com>
2025-02-15 13:36:05 +0700

    saya menambahkan sesuatu

--- .history/src/app/layout_20250215133407.tsx
+++ .history/src/app/layout_20250215133407.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/Components/Sidebar";
-import Footer from "@/Components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,7 +30,7 @@ export default function RootLayout({
       >
         <Sidebar/>
         {children}
-        <Footer/>
+        
       </body>
       <script src="node_modules\flowbite\dist\flowbite.min.js"></script>
     </html>
