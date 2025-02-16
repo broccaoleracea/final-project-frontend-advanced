f00170cfe17186e57397113d62ac18e8c81e65d3
kat <143475611+broccaoleracea@users.noreply.github.com>
2025-02-16 12:32:11 +0700

    Define data types

---
+++ src/types/dataTypes.ts
@@ -0,0 +1,51 @@
+export interface Kategori {
+    kategori_id: number;
+    kategori_nama: string;
+}
+
+export interface Alat {
+    alat_id: number;
+    alat_kategori_id: number;
+    alat_nama: string;
+    alat_deskripsi: string;
+    alat_hargaPerhari: number;
+    alat_stok: number;
+    kategori?: Kategori;
+}
+
+export interface Pelanggan {
+    pelanggan_id: number;
+    pelanggan_nama: string;
+    pelanggan_alamat: string;
+    pelanggan_notelp: string;
+    pelanggan_email: string;
+}
+
+export interface PelangganData {
+    pelanggan_data_id: number;
+    pelanggan_data_pelanggan_id: number;
+    pelanggan_data_jenis: "KTP" | "SIM";
+    pelanggan_data_file: string;
+    pelanggan?: Pelanggan;
+}
+
+export interface Penyewaan {
+    penyewaan_id: number;
+    penyewaan_pelanggan_id: number;
+    penyewaan_tglSewa: string;]
+    penyewaan_tglKembali: string;
+    status_pembayaran: "Lunas" | "Belum_Dibayar" | "DP";
+    status_pengembalian: "Sudah_Kembali" | "Belum_Kembali";
+    penyewaan_totalHarga: number;
+    pelanggan?: Pelanggan;
+}
+
+export interface PenyewaanDetail {
+    penyewaan_detail_id: number;
+    penyewaan_detail_penyewaan_id: number;
+    penyewaan_detail_alat_id: number;
+    penyewaan_detail_jumlah: number;
+    penyewaan_detail_subHarga: number;
+    penyewaan?: Penyewaan;
+    alat?: Alat; 
+}
