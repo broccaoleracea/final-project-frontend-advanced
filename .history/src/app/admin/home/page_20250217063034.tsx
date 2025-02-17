2b6ee8f3084af67ac4b9363baa68eb426d2a1d67
dausandhika <firdaus.andhika06@gmail.com>
2025-02-17 07:34:05 +0700

    saya menambahkan sesuatu

---
+++ .history/src/app/admin/home/page_20250217063034.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import HomeView from "./home.view";
+
+export default function Alat() {
+  return (
+    <div className="ml-64">
+      <HomeView />
+    </div>
+  );
+}
